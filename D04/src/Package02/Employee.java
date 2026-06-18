cc1e2ffef406fdb21ffeb3fd724b3353b64b5492
Adarsh Herwade <raosahebherwade123@gmail.com>
2026-06-18 15:30:47 +0530

    D04 Code

---
+++ D04/src/Package02/Employee.java
@@ -0,0 +1,51 @@
+package Package02;
+
+import java.util.Scanner;
+
+// Employee has-a DOJ
+public class Employee {
+	int empid;
+	String name;
+	double salary;
+	// Field Initializer
+	// Date doj = new Date(); // Associatation (Composition)
+	Date doj;// Associatation (Composition)
+	Date dot; // Asscoiatation (Aggegration)
+
+	public Employee() {
+		doj = new Date();
+	}
+
+	public void acceptEmployee(Scanner sc) {
+		System.out.print("Enter empid - ");
+		empid = sc.nextInt();
+
+		System.out.print("Enter name - ");
+		name = sc.next();
+
+		System.out.print("Enter salary - ");
+		salary = sc.nextDouble();
+
+		System.out.println("Enter the date of joining : ");
+		doj.acceptDate(sc);
+	}
+
+	public void terminateEmployee(Scanner sc) {
+		System.out.println("Enter the termination date - ");
+		dot = new Date();
+		dot.acceptDate(sc);
+	}
+
+	public void displayEmployee() {
+		System.out.println("Empid - " + empid);
+		System.out.println("Name - " + name);
+		System.out.println("Salary - " + salary);
+		System.out.print("Date of joining : ");
+		doj.displayDate();
+
+		if (dot != null) {
+			System.out.print("Date of termination : ");
+			dot.displayDate();
+		}
+	}
+}
---
+++ D04/src/Package02/Program01.java
@@ -0,0 +1,23 @@
+package Package02;
+
+import java.util.Scanner;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		Date d1 = new Date();
+//		d1.acceptDate(sc);
+//		d1.displayDate();
+
+		Employee e1 = new Employee();
+		e1.acceptEmployee(sc);
+		e1.displayEmployee();
+
+		Employee e2 = new Employee();
+		e2.acceptEmployee(sc);
+		e2.terminateEmployee(sc);
+		e2.displayEmployee();
+	}
+
+}
