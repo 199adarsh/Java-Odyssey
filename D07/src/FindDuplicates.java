8ad6bb467617086c561d2fd4ee52221dc4554a12
Adarsh Herwade <raosahebherwade123@gmail.com>
2026-06-22 16:57:23 +0530

    Update

--- D07/src/FindDuplicates.java
+++ D07/src/FindDuplicates.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class FindDuplicates {
 
     public void main(String[] args){
 
-        String[] s1 = {"Orange", "Mango", "Apple", "Strawberry","Pineapple"};
-        String[] s2 = {"Banana","Papaya","Orange","Jackfruit","Strawberry",};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of Array");
+        int size =sc.nextInt();
+
+        String[] s1 = new String[size] ;
+        String[] s2 = new String[size] ;
+        for(int i = 0; i < size; i++) {
+            System.out.print("Enter value for S1 :");
+            s1[i] = sc.next();
+            System.out.print("Enter value for S2 :");
+            s2[i] = sc.next();
+            System.out.println("");
+        }
 
         for(String s : s1){
             for(String x : s2){
