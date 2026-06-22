ade69658356ee31f5ae74ba0db381012d925c48a
Adarsh Herwade <raosahebherwade123@gmail.com>
2026-06-22 16:28:50 +0530

    Update

--- D07/src/Palindrome.java
+++ D07/src/Palindrome.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
-public class Pallindrome {
+public class Palindrome {
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a String to check pallindrome : ");
+        System.out.print("Enter a String to check palindrome : ");
         String s1 = sc.next();
 
         for(int i = 0; i < s1.length()/2; i++) {
             if(s1.charAt(i) != s1.charAt(s1.length()-i-1)){
-                System.out.println("This is not a Pallindrome !");
+                System.out.println("This is not a Palindrome !");
                 return;
             }
         }
-        System.out.println("This is a a Pallindrome");
+        System.out.println("This is a a Palindrome");
 
     }
 }
