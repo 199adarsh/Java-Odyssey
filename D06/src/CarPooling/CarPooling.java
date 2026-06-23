f1c6fdb7610139460ac8c9083a1ad568e289b6c5
Adarsh Herwade <raosahebherwade123@gmail.com>
2026-06-23 16:40:00 +0530

    Day 06: Implement CarPooling in CarPooling

---
+++ D06/src/CarPooling/CarPooling.java
@@ -0,0 +1,70 @@
+package CarPooling;
+
+public class CarPooling {
+	double milesDriven;
+	double gallonCost;
+	double averageMileage;
+	double parkingFees;
+	double tolls;
+	double vehicleCost;
+
+	public CarPooling () {}
+
+	public CarPooling (double averageMileage, double gallonCost, double milesDriven, double parkingFees, double tolls) {
+		this.averageMileage = averageMileage;
+		this.gallonCost = gallonCost;
+		this.milesDriven = milesDriven;
+		this.parkingFees = parkingFees;
+		this.tolls = tolls;
+	}
+
+
+	void vehicleCost (){
+		vehicleCost = (milesDriven/ averageMileage)*gallonCost;
+		System.out.println ("Vehicle Cost : " +vehicleCost);
+
+	}
+	void calculateTotalCost(){
+		System.out.println ("Total Cost : "+ (vehicleCost+parkingFees+tolls));
+	}
+
+	public double getAverageMileage () {
+		return averageMileage;
+	}
+
+	public void setAverageMileage (double averageMileage) {
+		this.averageMileage = averageMileage;
+	}
+
+	public double getGallonCost () {
+		return gallonCost;
+	}
+
+	public void setGallonCost (double gallonCost) {
+		this.gallonCost = gallonCost;
+	}
+
+	public double getMilesDriven () {
+		return milesDriven;
+	}
+
+	public void setMilesDriven (double milesDriven) {
+		this.milesDriven = milesDriven;
+	}
+
+	public double getParkingFees () {
+		return parkingFees;
+	}
+
+	public void setParkingFees (double parkingFees) {
+		this.parkingFees = parkingFees;
+	}
+
+	public double getTolls () {
+		return tolls;
+	}
+
+	public void setTolls (double tolls) {
+		this.tolls = tolls;
+	}
+}
