709e4eb5e4dfb7168dda16420542a9d2387837e3
Adarsh Herwade <raosahebherwade123@gmail.com>
2026-07-01 13:24:00 +0530

    Day 12: Implement Program01 in Package01

---
+++ D12/src/Package01/Program01.java
@@ -0,0 +1,57 @@
+package Package01;
+
+//  java 8
+// interfaces are mutable
+interface Acceptable {
+	void accept();
+
+	default void display() {
+		System.out.println("Acceptable::display");
+	}
+}
+
+class Employee implements Acceptable {
+	@Override
+	public void accept() {
+		System.out.println("Employee::accept");
+	}
+
+	@Override
+	public void display() {
+		System.out.println("Employee::display");
+	}
+
+}
+
+class Date implements Acceptable {
+	@Override
+	public void accept() {
+		System.out.println("Date::accept");
+	}
+}
+
+class Product implements Acceptable {
+	@Override
+	public void accept() {
+		System.out.println("Product::accept");
+	}
+
+	@Override
+	public void display() {
+		Acceptable.super.display();
+		System.out.println("Product::display");
+	}
+
+}
+
+public class Program01 {
+	public static void main(String[] args) {
+		Acceptable a1;
+//		a1 = new Employee();
+//		a1 = new Date();
+		a1 = new Product();
+		a1.accept();
+		a1.display();
+	}
+
+}
