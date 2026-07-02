78480d0ae4c6d45ade7918cc4189749a6ffdd87c
Adarsh Herwade <raosahebherwade123@gmail.com>
2026-07-02 13:32:00 +0530

    Day 12: Implement Program04 in Package02

---
+++ D12/src/Package02/Program04.java
@@ -0,0 +1,34 @@
+package Package02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program04 {
+
+	public static void main(String[] args) {
+		List<Employee> employees = new ArrayList<>();
+		employees.add(new Employee(5, "Mukesh", 20000));
+		employees.add(new Employee(1, "Ramesh", 40000));
+		employees.add(new Employee(4, "Anil", 30000));
+		employees.add(new Employee(2, "Suresh", 50000));
+		employees.add(new Employee(3, "Ram", 10000));
+
+		System.out.println("unsorted employees -> ");
+		for (Employee e : employees)
+			System.out.println(e);
+		System.out.println("------------------------------");
+
+		System.out.println("employees sorted on empid -> ");
+
+		// Lambda expression : short hand implementation of Functional Interface
+		// -> : Lambda operator
+		employees.sort((Employee o1, Employee o2) -> {
+			return o1.empid - o2.empid;
+		});
+
+		for (Employee e : employees)
+			System.out.println(e);
+		System.out.println("------------------------------");
+	}
+
+}
