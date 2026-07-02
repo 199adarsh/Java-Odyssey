b050ce2bb3c0ed2ce714e806cc8677256e010d6c
Adarsh Herwade <raosahebherwade123@gmail.com>
2026-07-02 13:24:00 +0530

    Day 12: Implement Program03 in Package02

---
+++ D12/src/Package02/Program03.java
@@ -0,0 +1,37 @@
+package Package02;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Program03 {
+
+	public static void main(String[] args) {
+		List<Employee> employees = new ArrayList<>();
+		employees.add(new Employee(5, "Mukesh", 20000));
+		employees.add(new Employee(1, "Ramesh", 40000));
+		employees.add(new Employee(4, "Anil", 30000));
+		employees.add(new Employee(2, "Suresh", 50000));
+		employees.add(new Employee(3, "Ram", 10000));
+
+		System.out.println("unsorted employees -> ");
+		for (Employee e : employees)
+			System.out.println(e);
+		System.out.println("------------------------------");
+
+		System.out.println("employees sorted on empid -> ");
+
+		// Anonymous object of Anonymous class
+		employees.sort(new Comparator<Employee>() {
+			@Override
+			public int compare(Employee o1, Employee o2) {
+				return o1.empid - o2.empid;
+			}
+		});
+
+		for (Employee e : employees)
+			System.out.println(e);
+		System.out.println("------------------------------");
+	}
+
+}
