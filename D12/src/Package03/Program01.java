d15fc5ea14b4dbdeb28f62042d35c8db967f2b60
Adarsh Herwade <raosahebherwade123@gmail.com>
2026-07-03 13:08:00 +0530

    Day 12: Implement Program01 in Package03

---
+++ D12/src/Package03/Program01.java
@@ -0,0 +1,18 @@
+package Package03;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		Integer[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		// display square of all even numbers
+		for (Integer e : arr) {
+			if (e % 2 == 0)// operation 1 : To check for even number (check the condition use filter)
+			{
+				int sq = e * e;// operation 2: Calculate the square of a number (To perform operation on
+								// elements use map)
+				System.out.println("Square - " + sq); // operation 3 : display the result
+			}
+		}
+	}
+
+}
