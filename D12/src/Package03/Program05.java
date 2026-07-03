ba2c4650e6bd84e7b882cdde3c374bca43339f33
Adarsh Herwade <raosahebherwade123@gmail.com>
2026-07-03 13:40:00 +0530

    Day 12: Implement Program05 in Package03

---
+++ D12/src/Package03/Program05.java
@@ -0,0 +1,24 @@
+package Package03;
+
+import java.util.stream.Stream;
+
+public class Program05 {
+
+	public static void main(String[] args) {
+		String[] arr = { "Anil", "Mukesh", "Ramesh", "Suresh", "Ram", "Sham", "Anil", "Mukesh", "Ramesh", "Suresh",
+				"Ram", "Sham" };
+
+		Stream<String> s1 = Stream.of(arr);
+
+		// count of all the elements
+		// System.out.println("no of elements - " + s1.count());
+
+		// display count of distinct names
+		// long uniqueNameCount = s1.distinct().count();
+		// System.out.println("Distinct elements count - " + uniqueNameCount);
+
+		// display distinct names
+		s1.distinct().forEach(System.out::println);
+	}
+
+}
