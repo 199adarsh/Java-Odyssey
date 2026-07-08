6116a88588810599145bf8cb217aeec008b986e8
Adarsh Herwade <raosahebherwade123@gmail.com>
2026-07-08 13:24:00 +0530

    Day 14: Implement SyncThread in p2

---
+++ D14/src/p2/SyncThread.java
@@ -0,0 +1,57 @@
+package p2;
+
+public class SyncThread {
+
+	public static void delay() {
+		try {
+			Thread.sleep(300);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		BankAccount a1 = new BankAccount(1001, 10000);
+
+		class DepositThread extends Thread {
+			@Override
+			public void run() {
+				for (int i = 0; i < 10; i++) {
+					synchronized (a1) {
+						a1.deposit(1000);
+					}
+					System.out.println("After deposit balance - " + a1.balance);
+					delay();
+				}
+			}
+		}
+
+		DepositThread dt = new DepositThread();
+		dt.start();
+
+		class WithdrawThread extends Thread {
+			@Override
+			public void run() {
+				for (int i = 0; i < 10; i++) {
+					synchronized (a1) {
+						a1.withdraw(1000);
+					}
+					System.out.println("After withdraw balance - " + a1.balance);
+					delay();
+				}
+			}
+		}
+
+		WithdrawThread wt = new WithdrawThread();
+		wt.start();
+
+		try {
+			dt.join();
+			wt.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Final Balance : " + a1.balance);
+	}
+}
